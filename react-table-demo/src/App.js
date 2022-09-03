7f7d7b03bea6015a4fe927a1dce50a1bbf692a70
hnz989 <zhenghualin2008@gmail.com>
2022-09-03 00:55:08 -0400

    Use FilteringTable

--- react-table-demo/src/App.js
+++ react-table-demo/src/App.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import './App.css'
-import { BasicTable } from './components/BasicTable'
+// import {BasicTable} from './components/BasicTable'
+// import {Checkbox} from './components/Checkbox'
+import {FilteringTable} from './components/FiltertingTable'
 
 function App() {
-  return (
-    <div className='App'>
-      <BasicTable />
-    </div>
-  )
+    return (
+        <div className='App'>
+            <FilteringTable/>
+        </div>
+    )
 }
 
 export default App
